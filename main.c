c3df61f5350d1a0351c446c49a457a46945fea41
Micky Sec <mikonsi@proton.me>
2026-05-03 21:16:40 -0400

    added the math and very basic validation. Now a working, but basic, version

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <math.h>
 
-// Compund Interest Formula: A = P(1+r/n)^n*t
-
 /* argc is the num of arguments to be passed
 argv is a reference to an array of pointers to each argument passed to the program
 ** argv[0] is always the program name. Therefore if user enters 4 arguments then argc = 1+ 5
@@ -11,18 +9,26 @@ argv is a reference to an array of pointers to each argument passed to the progr
 int main(int argc, char *argv[]) {
 
   if (argc != 5) {
-    fprintf(stderr, "Invalid Inputs. Enter:\n principal, \nrate(as decimal) \ncompoundFrequency \ntime(years)");
-    return 3;
+    fprintf(stderr, "Invalid Inputs. Enter:\nprincipal, \nrate(as decimal) \ncompoundFrequency (# of times interest is compounded per year) \ntime(years)");
+    return 1;
   }
-
-  //TODO: Validate inputs:
   
   // NULL because strtod (string to double) must ignore any advanced error
-  double principal = strtod(argv[1], NULL);
-  double rate = strtod(argv[2], NULL);
-  double compoundFrequency = strtod(argv[3], NULL);
-  double time = strtod(argv[4], NULL); 
+  double p = strtod(argv[1], NULL);
+  double r = strtod(argv[2], NULL);
+  double cF = strtod(argv[3], NULL);
+  double t = strtod(argv[4], NULL); 
 
-  //TODO: Implement actual math
-  
+
+  if(p < 0 || r < 0 || cF < 0 || cF != 0 || t < 0){
+    fprintf(stderr, "Values must be greater than 0");
+    return 1;
+  } 
+
+
+  double A;
+  A = p*pow((1+(r/cF)),(cF*t));
+  printf("Final Amount With Interest: %.2f\n", A);
+
+  return 0;
 }
